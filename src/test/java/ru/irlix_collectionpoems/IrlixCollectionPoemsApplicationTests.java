e306e07e996c2e7089d1babfe631101b72261b62
Alexandr <amakutsevich@bk.ru>
2024-08-19 14:56:13 +0700

    Init commit

---
+++ src/test/java/ru/irlix_collectionpoems/IrlixCollectionPoemsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.irlix_collectionpoems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrlixCollectionPoemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
